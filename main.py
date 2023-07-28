13a7a9aae23cc64c8dea6ba42c6dcbba39a7f82c
HarshPriyesh <92246846+HarshPriyesh@users.noreply.github.com>
2023-07-29 02:40:31 +0530

    config 2 file creation using catalog json

--- main.py
+++ main.py
@@ -117,6 +117,8 @@ def convert_schema_to_json(dirpath, file):
     temp_hql = open("temp.hql", "r")
     skip_mode = False
     jsonCount = 0
+    entryNames = []
+    locations = []
     for line in temp_hql:
         wordsInLine = line.lstrip().rstrip().replace("`", "").split(" ")
         if "create " in line.lower():
@@ -126,6 +128,7 @@ def convert_schema_to_json(dirpath, file):
             print(f"-----\nConversion {jsonCount+1}")
             print(f"INFO  :  Table Name - {tableName}")
             DBtableName = shorten_name(DBtableName)
+            entryNames.append(DBtableName)
             catalogJson = open(os.path.join(destination, DBtableName + ".json"), "w")
             jsonCount += 1
             print(f'INFO  :  Creating Catalog Json, {DBtableName + ".json"}')
@@ -142,6 +145,20 @@ def convert_schema_to_json(dirpath, file):
                 '  "schema": {"columns": [\n',
             ]
             catalogJson.writelines(l)
+        elif "location " in line.lower():
+            if "'gs://" in line.lower():
+                locations.append(
+                    line.replace("LOCATION", "")
+                    .replace(r"${hivevar:stagingBucket}", f"{bucket_name}")
+                    .replace("'", "")
+                    .replace(";", "")
+                    .replace("\n", "")
+                    .lstrip()
+                )
+            else:
+                locations.append(
+                    f"gs://{bucket_name}/{feedname}_{trouxid}/{feedname}/{tableName}/"
+                )
         else:
             if "partitioned by" in line.lower():
                 l1 = ["  ]}\n", "}"]
@@ -187,23 +204,27 @@ def convert_schema_to_json(dirpath, file):
         print(f"ERROR :  An error occurred while deleting the file: {e}")
     print(f"INFO  :  Total Catalog Json created: {jsonCount}")
     print(f"INFO  :  Number of table names trimmed: {trim_name}")
+    if config_2:
+        config2(entryNames, locations)
 
 
 def config1(destination, config_file):
-    GCP_AtlasJson_path = yaml_data.get("GCP_AtlasJson_path")
     print(f'INFO  :  GCP_AtlasJson_path - "{GCP_AtlasJson_path}"')
-    GCP_CatalogJson_path = yaml_data.get("GCP_CatalogJson_path")
     print(f'INFO  :  GCP_CatalogJson_path - "{GCP_CatalogJson_path}"')
     print("INFO  :  Creating/Updating...data_catalog_create_schema_json_config.txt")
     count = 0
-    atlasJson_names = []
+    entryNames = []
+    locations = []
     with open(
         config_file + "/data_catalog_create_schema_json_config.txt", "w"
     ) as config1:
         config1.write("atlas_ectract,dest_storage_path\n")
         for filename in os.listdir(destination):
             if os.path.isfile(os.path.join(destination, filename)):
-                atlasJson_names.append(filename.replace(".json", ""))
+                entryNames.append(filename.replace(".json", ""))
+                locations.append(
+                    f"gs://{bucket_name}/{feedname}_{trouxid}/{feedname}/{filename.split('.')[1]}/"
+                )
                 c1 = (
                     GCP_AtlasJson_path
                     + "/"
@@ -217,34 +238,36 @@ def config1(destination, config_file):
                 config1.write(c1)
                 count += 1
     print(f"INFO  :  Total lines updated in config1 file: {count}")
-    if bool(yaml_data.get("config2").lower() == "true"):
-        print("-----\nINFO  :  Config 2 execution")
-        entry_group = yaml_data.get("entry_group")
-        print(f'INFO  :  entry_group - "{entry_group}"')
-        bucket_name = yaml_data.get("bucket_name")
-        print(f'INFO  :  bucket_name - "{bucket_name}"')
-        print(
-            f'INFO  :  Config files path: "{config_file}/data_catalog_create_ext_entries_config.txt"'
-        )
-        print("INFO  :  Creating/Updating...data_catalog_create_ext_entries_config.txt")
-        count = 0
-        with open(
-            config_file + "/data_catalog_create_ext_entries_config.txt", "w"
-        ) as config2:
-            config2.write("entry_group,entry,schema_path,linked_resources\n")
-            for names in atlasJson_names:
-                c2 = (
-                    entry_group
-                    + ","
-                    + names
-                    + ","
-                    + GCP_CatalogJson_path
-                    + ","
-                    + f"gs://{bucket_name}/{feedname}_{trouxid}/{feedname}/{names.split('.')[1]}/\n"
-                )
-                config2.write(c2)
-                count += 1
-        print(f"INFO  :  Total lines updated in config2 file: {count}")
+    if config_2:
+        config2(entryNames, locations)
+
+
+def config2(entryNames, locations):
+    print("-----\nCONFIG 2 execution")
+    print(f'INFO  :  entry_group - "{entry_group}"')
+    print(f'INFO  :  bucket_name - "{bucket_name}"')
+    print(
+        f'INFO  :  Config files path: "{config_file}/data_catalog_create_ext_entries_config.txt"'
+    )
+    print("INFO  :  Creating/Updating...data_catalog_create_ext_entries_config.txt")
+    count = 0
+    with open(
+        config_file + "/data_catalog_create_ext_entries_config.txt", "w"
+    ) as config2:
+        config2.write("entry_group,entry,schema_path,linked_resources\n")
+        for name, location in zip(entryNames, locations):
+            c2 = (
+                entry_group
+                + ","
+                + name
+                + ","
+                + GCP_CatalogJson_path
+                + ","
+                + f"{location}\n"
+            )
+            config2.write(c2)
+            count += 1
+    print(f"INFO  :  Total lines updated in config2 file: {count}")
 
 
 if __name__ == "__main__":
@@ -277,7 +300,6 @@ if __name__ == "__main__":
         "timestamp",
         "boolean",
     ]
-
     yaml_file = "parameter.yaml"
     yaml_data = read_yaml_file(yaml_file)
     feedname = yaml_data.get("feedname")
@@ -285,7 +307,13 @@ if __name__ == "__main__":
     database = yaml_data.get("schema_database")
     rawDB = yaml_data.get("raw_database")
     hqlFile = yaml_data.get("hql")
-    atlasJson = bool(yaml_data.get("config1").lower() == "true")
+    config_1 = bool(yaml_data.get("config1").lower() == "true")
+    config_2 = bool(yaml_data.get("config2").lower() == "true")
+    entry_group = yaml_data.get("entry_group")
+    bucket_name = yaml_data.get("bucket_name")
+    GCP_CatalogJson_path = yaml_data.get("GCP_CatalogJson_path")
+    GCP_AtlasJson_path = yaml_data.get("GCP_AtlasJson_path")
+
     print("\nEXECUTION STARTED")
     print("*******")
     print(f"Feed Name: {feedname}")
@@ -293,7 +321,7 @@ if __name__ == "__main__":
     print(f"Schema Database: {database}")
     print(f"Raw Database: {rawDB}")
     print(f"HQL: {hqlFile}")
-    if atlasJson:
+    if config_1:
         destination = yaml_data.get("atlasJson_path")
         print(f'Atlas_Json_Path: "{destination}"')
     else:
@@ -302,7 +330,7 @@ if __name__ == "__main__":
     print("*******")
     if not os.path.exists(destination):
         print(f'INFO  :  Path dose not exist, "{destination}"')
-        if atlasJson:
+        if config_1:
             print("ERROR :  Mention the correct Atlas Json path to proceed")
             print("*******\nEXECUTION FAILED")
             restore_stdout(stdout_original)
@@ -310,25 +338,22 @@ if __name__ == "__main__":
         print(f"INFO  :  Creating directory...")
         os.makedirs(destination)
         print(f'INFO  :  Path directory created, "{destination}"')
-    if atlasJson:
-        print("INFO  :  Config 1 execution")
+    config_file = r"./Configs/" + feedname
+    if config_1 or config_2:
+        if not os.path.exists(config_file):
+            print(f'INFO  :  Config path dose not exist, "{config_file}"')
+            print(f"INFO  :  Creating directory...")
+            os.makedirs(config_file)
+            print(f'INFO  :  Config directory created, "{config_file}"')
+    if config_1:
+        print("CONFIG 1 execution")
         if len(os.listdir(destination)) == 0:
             print(f"ERROR :  The directory is empty, {destination}")
             print("*******\nEXECUTION FAILED")
             restore_stdout(stdout_original)
             exit()
-        else:
-            config_file = r"./Configs/" + feedname
-            if not os.path.exists(config_file):
-                print(f'INFO  :  Config path dose not exist, "{config_file}"')
-                print(f"INFO  :  Creating directory...")
-                os.makedirs(config_file)
-                print(f'INFO  :  Config directory created, "{config_file}"')
-            else:
-                print(
-                    f'INFO  :  Config files path: "{config_file}/data_catalog_create_schema_json_config.txt"'
-                )
-            config1(destination, config_file)
+        print(f'INFO  :  Config files path: "{config_file}/data_catalog_create_schema_json_config.txt"')
+        config1(destination, config_file)
     else:
         checkHQL = True
         for dirpath, dirnames, filenames in os.walk(source):
